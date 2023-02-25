ff7e1f01c6c2159cf8ae47c88e9ebb47a57dedb8
Cody Kilpatrick <codykilpatrick1996@gmail.com>
2023-02-25 14:33:20 -0500

    add map to points list page

--- src/components/PointsList/PointsList.tsx
+++ src/components/PointsList/PointsList.tsx
@@ -1,5 +1,6 @@
 // npm packages
 import { useState, useEffect } from 'react';
+import Map from "react-map-gl";
 
 // services
 import * as pointService from '../../services/pointservice'
@@ -13,6 +14,13 @@ import NewPointForm from '../NewPointForm/NewPointForm';
 
 const PointsList = (): JSX.Element => {
   const [points, setPoints] = useState<Point[]>([])
+  let [viewport, setViewport] = useState({
+    latitude: 37.7,
+    longitude: -122,
+    zoom: 8,
+    width: innerWidth,
+    height: innerHeight
+  })
 
   useEffect((): void => {
     const fetchPoints = async (): Promise<void> => {
@@ -38,6 +46,16 @@ const PointsList = (): JSX.Element => {
 
   return (
     <>
+      <Map
+      mapboxAccessToken='pk.eyJ1IjoiY29keWtpbHBhdHJpY2siLCJhIjoiY2xla2NkZXhmMGdyeDQzbWxkd3Flc2UxbCJ9.YDQHBecK22YVpggfTsAsLA'
+        initialViewState={{
+          longitude: -100,
+          latitude: 40,
+          zoom: 3.5
+        }}
+        style={{width: 600, height: 400}}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+      />
       <h1>Measurements</h1>
       <NewPointForm handleAddPoint={handleAddPoint}/>
       {points.map((point: Point) =>
