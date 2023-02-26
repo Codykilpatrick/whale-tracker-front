06fc64aa435a6724103792ef536d35c5d8aa5412
Cody Kilpatrick <codykilpatrick1996@gmail.com>
2023-02-26 13:38:50 -0500

    add content to the landing page

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -6,7 +6,6 @@ import { User } from '../../types/models'
 
 // components
 import PointsList from '../../components/PointsList/PointsList';
-import ProtectedRoute from '../../components/ProtectedRoute/ProtectedRoute';
 
 interface LandingProps {
   user: User | null;
@@ -23,18 +22,26 @@ const Landing = (props: LandingProps): JSX.Element => {
     )
   }
   return (
-    <>
+    <main className={styles.notLoggedIn}>
       <h1>Welcome to whale tracker!</h1>
-      <article>
-        Whale Tracker is the perfect tool for anyone who needs to access ocean data quickly and easily. By connecting to an ocean data API, the app retrieves real-time information on ocean temperature, salinity, and depth for any location in the world.
-
-        One of the app's most exciting features is its ability to generate sound speed profiles, which are graphs that display sound speed for a given depth! With Whale Tracker, users can quickly and easily generate these graphs to help them understand how sound behaves in different parts of the ocean.
-
-        In addition to providing data on ocean conditions, Whale Tracker also allows users to create and save points on a map. This feature makes it easy for users to keep track of locations they are interested in or have visited in the past.
-
-        Whale Tracker is perfect for anyone who needs to access ocean data. Whether they're a professional oceanographer or just a curious beachgoer. With its easy-to-use interface and powerful data capabilities, it's the perfect tool for anyone who wants to explore the mysteries of the ocean.
-      </article>
-    </>
+      <div>
+        <img src="https://i.imgur.com/TaCX6n3.png" alt="Whale" />
+        <article>
+          <p>
+            Whale Tracker is the perfect tool for anyone who needs to access ocean data quickly and easily. By connecting to an ocean data API, the app retrieves real-time information on ocean temperature, salinity, and depth for any location in the world.
+          </p>
+          <p>
+            One of the app's most exciting features is its ability to generate sound speed profiles, which are graphs that display sound speed for a given depth! With Whale Tracker, users can quickly and easily generate these graphs to help them understand how sound behaves in different parts of the ocean.
+          </p>
+          <p>
+            In addition to providing data on ocean conditions, Whale Tracker also allows users to create and save points on a map. This feature makes it easy for users to keep track of locations they are interested in or have visited in the past.
+          </p>
+          <p>
+            Whale Tracker is perfect for anyone who needs to access ocean data. Whether they're a professional oceanographer or just a curious beachgoer. With its easy-to-use interface and powerful data capabilities, it's the perfect tool for anyone who wants to explore the mysteries of the ocean.
+          </p>
+        </article>
+      </div>
+    </main>
   )
 }
 
